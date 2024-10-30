64e9fdf9217f8c1d73f1de8cc3b87331603e93a6
unknown <smhaseebali8@gmail.com>
2024-10-30 09:12:48 +0500

    synchronized models with database using sequelize.sync() and removed db.execute()

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const db = require("./util/database")
+const sequelize = require("./util/database") 
 
 const app = express();
 
@@ -14,9 +14,9 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-db.execute('SELECT * FROM products')
-  .then(result => console.log(result[0][0].title))
-  .catch(err => err)
+// db.execute('SELECT * FROM products')
+//   .then(result => console.log(result[0][0].title))
+//   .catch(err => err)
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -25,6 +25,8 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
+
+sequelize.sync().then(result=> console.log(1)).catch(err=> console.log(err))
 
 app.listen(3000, ()=>{
   console.log(`Server live at 3000`)
