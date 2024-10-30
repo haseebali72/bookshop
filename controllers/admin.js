5b81e2488b0087399540aa82aa229d7252b8d0bd
unknown <smhaseebali8@gmail.com>
2024-10-31 04:26:04 +0500

    removed manually created Product model methods and added sequelize methods

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,48 +1,57 @@
+const { where } = require('sequelize');
 const Product = require('../models/product');
 
+
+//a get request to render the add-product page
 exports.getAddProduct = (req, res, next) => {
-  res.render('admin/edit-product', {
+  res.render('admin/add-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
     editing: false
   });
 };
 
+//  a post request to send the product data on server to be saved by db 
 exports.postAddProduct = (req, res, next) => {
   const title = req.body.title;
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
   Product.create({
-    title : title,
-    price : price,
-    imageUrl : imageUrl,
-    description : description
+    title: title,
+    price: price,
+    imageUrl: imageUrl,
+    description: description
   })
-  .then()
-  .catch()
- 
+    .then()
+    .catch()
+
   res.redirect('/');
 };
 
+// get request to render the edit product page
 exports.getEditProduct = (req, res, next) => {
   const editMode = req.query.edit;
   if (!editMode) {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findById(prodId, product => {
-    if (!product) {
-      return res.redirect('/');
-    }
-    res.render('admin/edit-product', {
-      pageTitle: 'Edit Product',
-      path: '/admin/edit-product',
-      editing: editMode,
-      product: product
-    });
-  });
+  Product.findByPk(prodId)
+    .then(product => {
+      if (!product) {
+        return res.redirect('/');
+      }
+      res.render('admin/edit-product', {
+        pageTitle: 'Edit Product',
+        path: '/admin/edit-product',
+        editing: editMode,
+        product: product
+      });
+    })
+    .catch(err => console.log(err));
 };
+
+// post request to send data of the edited product to the server
 
 exports.postEditProduct = (req, res, next) => {
   const prodId = req.body.productId;
@@ -50,29 +59,41 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-  const updatedProduct = new Product(
-    prodId,
-    updatedTitle,
-    updatedImageUrl,
-    updatedDesc,
-    updatedPrice
-  );
-  updatedProduct.save();
-  res.redirect('/admin/products');
+  Product.findByPk(prodId)
+  .then(product=>{
+    product.title = updatedTitle
+    product.price = updatedPrice
+    product.imageUrl = updatedImageUrl
+    product.description = updatedDesc
+    return product.save()
+  })
+  .then(result => {
+    console.log(result, "Product Updated")
+    res.redirect("/")
+  })
+  .catch(err => console.log(err))
 };
 
+
+
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('admin/products', {
-      prods: products,
-      pageTitle: 'Admin Products',
-      path: '/admin/products'
-    });
-  });
+  Product.findAll()
+    .then(products => {
+      res.render('admin/products', {
+        prods: products,
+        pageTitle: 'Admin Products',
+        path: '/admin/products'
+      });
+    })
+    .catch(err => console.log(err));
 };
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId);
+  Product.destroy({
+    where : {
+      id : prodId
+    }
+  });
   res.redirect('/admin/products');
 };
